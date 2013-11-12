9bf63afc59ff288f0e80ad88f4581b213cdf250b
DON who is a ghost <therocketforever@me.com>
2013-11-12 13:10:04 -0500

    Converted inline ERB to Slim template. Broke out Inline template to layout.slim & index.slim

--- lib/application.rb
+++ lib/application.rb
@@ -31,7 +31,7 @@ class Application < Sinatra::Base
 
   get '/' do
     unless request.websocket?
-      erb :index
+      slim :index
     else
       request.websocket do |ws|
         ws.onopen do
@@ -51,40 +51,3 @@ class Application < Sinatra::Base
 end
 
 #binding.pry
-
-__END__
-@@ index
-<html>
-  <body>
-     <h1>Simple Echo & Chat Server</h1>
-     <form id="form">
-       <input type="text" id="input" value="send a message"></input>
-     </form>
-     <div id="msgs"></div>
-  </body>
-
-  <script type="text/javascript">
-    window.onload = function(){
-      (function(){
-        var show = function(el){
-          return function(msg){ el.innerHTML = msg + '<br />' + el.innerHTML; }
-        }(document.getElementById('msgs'));
-
-        var ws       = new WebSocket('ws://' + window.location.host + window.location.pathname);
-        ws.onopen    = function()  { show('websocket opened'); };
-        ws.onclose   = function()  { show('websocket closed'); }
-        ws.onmessage = function(m) { show('websocket message: ' +  m.data); };
-
-        var sender = function(f){
-          var input     = document.getElementById('input');
-          input.onclick = function(){ input.value = "" };
-          f.onsubmit    = function(){
-            ws.send(input.value);
-            input.value = "send a message";
-            return false;
-          }
-        }(document.getElementById('form'));
-      })();
-    }
-  </script>
-</html>
